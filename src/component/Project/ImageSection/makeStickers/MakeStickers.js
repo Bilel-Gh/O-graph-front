54a8ff858ef887dbb4d35f3506f8c6b531f80285
LaurentNiasme <laurent.niasme@gmail.com>
2020-09-27 13:52:37 +0200

    intégration de l image avec le createStickers, mise en page du form new message

--- src/component/Project/ImageSection/makeStickers/MakeStickers.js
+++ src/component/Project/ImageSection/makeStickers/MakeStickers.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import './makeStickers.css'
 
 const MakeStickers = () => {
+    // le hook useRef permet de contenir les propriété d une div dans une constante comme un document.getElementById en plus simple
+    // suffit de metre ref={la constante ref créé en amont} et s amuser avec la constante
+    const imgRef = useRef(null);
+    //detecter la fin du chargement de l'image pour pouvoir lancer d'autre function
+    const handleImageLoaded = () => {
+
+    }
+    // lors du clique sur l image on lance la sauvegarde du stickers dans le reducer
+    const saveStickers = () => {
+
+    }
+    // on relance le rendu de l image pour afficher le nouveau tableau de stickers
+    const createStickers = () => {
+
+    }
     return (
-        <div> </div>
+        <div className="main-image">
+          <div className='container-stickers-image'  ref={imgRef} >
+            <img className='image-comment' onLoad={handleImageLoaded}  src="https://intrld.com/wp-content/uploads/2020/05/one-piece-.png.webp" onClick={saveStickers} onContextMenu={(e)=>e.preventDefault()} />
+            {createStickers()}
+        </div>
+       </div>
 
     )
 }
---
+++ src/component/Project/ImageSection/newMessage/listCommentCreated/CommentCreated.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CommentCreated = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CommentCreated
