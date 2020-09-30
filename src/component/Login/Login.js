a1f165e6eace97d9c8a7933f77dedd0a23d35e61
Bilel-Gh <bilelghandri@outlook.fr>
2020-09-30 12:12:44 +0200

    login page input dynamic

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import { Input } from 'semantic-ui-react';
+import {useSelector, useDispatch} from 'react-redux';
+import{onLoginInput, onPasswordInput, sendLogin} from '../../store/loginSlice'
 import './login.css'
 
 
     const Login = () => {
+        const LoginState = useSelector(state => state.loginSlice);
+        const dispatch = useDispatch();
+
+        const onSubmitLogin = (e) => {
+            e.preventDefault()
+        dispatch(sendLogin({
+            login : LoginState.login ,
+            password : LoginState.password
+        }))
+        }
+
+        const handleLoginInput= (e) =>{
+            e.preventDefault()
+            const loginInput = e.target.value
+            dispatch(onLoginInput(
+                loginInput
+            ))
+
+        }
+
+        const handlePasswordInput= (e) =>{
+            e.preventDefault()
+            const passwordInput = e.target.value
+            dispatch(onPasswordInput(
+                passwordInput
+            ))
+
+         }
+
         return (
             <div className="loginPage">
                    <div className="loginDiv">
-                        
+
+                        <form className="login-Form" onSubmit={onSubmitLogin}>
+                            <Input
+                            className="login-Chat-User"
+                            type="text"
+                            placeholder=""
+                            onChange={handleLoginInput}
+                            value= {LoginState.login}
+                            >
+                            </Input>
+
+                            <Input
+                            className="login-Chat-Password"
+                            type="password"
+                            onChange={handlePasswordInput}
+                            value= {LoginState.password}
+                            >
+                            </Input>
+                            <button className="button-Login" type= "submit">Se Connecter</button>
+
+                        </form>
                    </div>
             </div>
         )
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import messageSlice from './messageSlice';
 import imageSlice from './imageSlice';
+import loginSlice from './loginSlice';
 
 const reducer = combineReducers({
-    messageSlice, imageSlice,
+    messageSlice, imageSlice, loginSlice,
 })
 
 const store = configureStore({
