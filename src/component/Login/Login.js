e7ad9909e99fc4ea9c0f9fb5e4c643b7ac1c2e88
Bilel-Gh <bilelghandri@outlook.fr>
2020-09-30 18:20:41 +0200

    svg succes

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -11,12 +11,9 @@ import{onLoginInput, onPasswordInput, sendLogin} from '../../store/loginSlice'
 import {fetchUser} from '../../store/userSlice'
 import './login.css'
 
-
-
     const Login = () => {
         const LoginState = useSelector(state => state.loginSlice);
         const dispatch = useDispatch();
-
 
         const onSubmitLogin = (e) => {
             e.preventDefault()
@@ -49,7 +46,6 @@ import './login.css'
 
         return (
             <div className="loginPage">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#5000ca" fill-opacity="1" d="M0,224L48,218.7C96,213,192,203,288,170.7C384,139,480,85,576,101.3C672,117,768,203,864,208C960,213,1056,139,1152,122.7C1248,107,1344,149,1392,170.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
                   <div className="loginDiv">
 
                         <form className="login-Form" onSubmit={onSubmitLogin}>
