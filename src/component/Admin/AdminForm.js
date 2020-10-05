2a5d854515b0d8ce2e43ed276a4c5cb359f89c11
Bilel-Gh <bilelghandri@outlook.fr>
2020-10-05 18:56:33 +0200

    socket io and css

--- src/component/Admin/AdminForm.js
+++ src/component/Admin/AdminForm.js
@@ -6,21 +6,31 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import './admin.css'
 
-const LoginInput = () => {
+const AdminForm = () => {
 return (
-  <MDBContainer>
+  <MDBContainer className="test">
   <MDBRow>
     <MDBCol md="6">
       <form>
         <p className="h3 text-center mb-4">Connexion</p>
         <div className="my-white-text">
-        <MDBInput label="Email" icon="envelope" group type="email" 
+        <MDBInput label="Email de l'utilisateur" icon="envelope" group type="email" 
              />
-          <MDBInput label="Mot de passe" icon="lock" group type="password" />
+          <MDBInput label="Mot de passe de l'utilisateur" icon="lock" group type="password" />
+
+          <label icon="lock" >Choisir le role de l'utilisateur</label>
+          <br></br>
+            <select class="choisir le role de l'utilisateur">
+                <option value="1" selected>Client</option>
+                <option value="">Graphiste</option>
+            </select>
+
+
         </div>
         <div className="text-center">
-          <MDBBtn outline color="white">
+          <MDBBtn outline color="red">
             <Link to="/project"> Se connecter </Link>
             <MDBIcon far icon="paper-plane" className="ml-1" />
           </MDBBtn>
@@ -32,4 +42,4 @@ return (
 );
 };
 
-export default LoginInput;
+export default AdminForm;
